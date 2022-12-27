d591030346f294e61b15031819c24a0cd1c90ab8
Natalia Levenets <levenets.lot@gmail.com>
2022-12-27 18:45:44 +0200

    Img descriptions check
    Img descriptions check

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,17 +3,16 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryBox = document.querySelector('.gallery');
 
-const items = galleryItems.map(({ preview, original, description }) => {
+let items = galleryItems.map(({ preview, original, description }) => {
     return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
 }).join('');
 
 galleryBox.insertAdjacentHTML('beforeend', items);
 
-let images = new SimpleLightbox('.gallery a');
-galleryBox.onfullscreenchange('show.simplelightbox', function () {
+var images = new SimpleLightbox('.gallery a');
+images.on('show.simplelightbox', function () {
     images.options.captionsData = "alt";
     images.options.captionsDelay = 250;
     images.options.scrollZoom = false;
     images.options.scrollZoomFactor = 0;
-    console.log(images.options.captionsData);
 });
